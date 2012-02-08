75032a05db722abd812dc82267705e0d52b974dc
spackjlefish@gmail.com <spackjlefish@gmail.com@ae435935-a78b-c63e-f1f1-0351114e69a1>
2012-02-08 20:06:23 +0000

    Some Base Classes

---
+++ trunk/Organizer/src/edu/sru/nullstring/Data/ReminderCollection.java
@@ -0,0 +1,12 @@
+package edu.sru.nullstring.Data;
+
+import java.util.List;
+import java.util.Observable;
+
+
+public class ReminderCollection extends Observable {
+		
+	public List<ReminderType> Reminders;
+
+
+}
