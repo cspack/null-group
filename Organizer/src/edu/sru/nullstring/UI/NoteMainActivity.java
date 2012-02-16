b00990deef0f36847d2cb3fab0b185ea20cb7c1d
ericjelder <ericjelder@gmail.com>
2012-02-16 04:57:10 +0000

    <3

--- Organizer/src/edu/sru/nullstring/UI/NoteMainActivity.java
+++ Organizer/src/edu/sru/nullstring/UI/NoteMainActivity.java
@@ -4,7 +4,8 @@ import edu.sru.nullstring.R;
 import edu.sru.nullstring.R.layout;
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Window;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class NoteMainActivity extends Activity {
 
@@ -13,10 +14,42 @@ public class NoteMainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.note_main);
         
+        
+    }
+    
+    private static final int CREATE_MENU_ID = Menu.FIRST;
+    private static final int FONTSIZE_MENU_ID = Menu.FIRST + 1;
+    private static final int TEST_MENU_ID = Menu.FIRST + 2;
 
-    	
-    	//Remove title bar
-    	this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-    	
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        
+        menu.add(0, CREATE_MENU_ID, 0, "Create").setShortcut('3', 'c');
+        menu.add(0, FONTSIZE_MENU_ID, 0, "Font Size").setShortcut('4', 's');
+        menu.add(0, TEST_MENU_ID, 0, "Test").setShortcut('5', 'z');
+        return true;
+    }
+    
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        super.onPrepareOptionsMenu(menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case CREATE_MENU_ID:
+                //create note
+                return true;
+            case FONTSIZE_MENU_ID:
+                //adjust font size
+                return true;
+            case TEST_MENU_ID:
+                //Testing menu items
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
