e5f86c04606dca260feec6d963f125ae49f06581
spackjlefish@gmail.com <spackjlefish@gmail.com@ae435935-a78b-c63e-f1f1-0351114e69a1>
2012-04-25 23:24:20 +0000

    I bring you reminders. - <3 Spack Wyatt I gave you 2 months to do this, you nub.

--- trunk/Organizer/src/edu/sru/nullstring/Data/ReminderAdapter.java
+++ trunk/Organizer/src/edu/sru/nullstring/Data/ReminderAdapter.java
@@ -1,5 +1,6 @@
 package edu.sru.nullstring.Data;
 
+import java.sql.SQLException;
 import java.util.List;
 
 import com.j256.ormlite.android.apptools.OpenHelperManager;
@@ -8,6 +9,7 @@ import edu.sru.nullstring.LocadexApplication;
 import edu.sru.nullstring.R;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,11 +19,56 @@ import android.widget.TextView;
 public class ReminderAdapter extends ArrayAdapter<ReminderType> {
     private List<ReminderType> items;
     private Context context;
+    private DatabaseHelper helper;
     
-    public ReminderAdapter(Context context, int textViewResourceId, List<ReminderType> items) {
-            super(context, textViewResourceId, items);
-            this.items = items;
+    
+    private void reloadReminders()
+    {
+
+    	try {
+    		
+    		CategoryType curCat = helper.getCurrentCategory();
+    		if(curCat.getFixedType() == CategoryType.FixedTypes.All)
+    		{
+    			items = helper.getReminderDao().queryForAll();
+    		}
+    		else
+    		{
+    			items = helper.getReminderDao().queryForEq(ReminderType.CAT_ID_FIELD, curCat.getID());
+    		}
+    		
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+	    	return;
+		}
+    }
+    public ReminderAdapter(Context context, int textViewResourceId, DatabaseHelper helper) {
+            super(context, textViewResourceId);
+            this.helper = helper;
+            reloadReminders();
             this.context = context;
+    }
+
+    @Override
+	public void notifyDataSetChanged() {
+		// TODO Auto-generated method stub
+		super.notifyDataSetChanged();
+		// db update goes here.. see category type.
+		
+	}
+    
+
+    @Override
+	public int getCount() {
+
+    	// Add fixed items to item size.
+		return items.size();
+	}
+
+    @Override
+	public ReminderType getItem(int position) {
+    	return items.get(position);
     }
 
     @Override
@@ -30,7 +77,7 @@ public class ReminderAdapter extends ArrayAdapter<ReminderType> {
             View v = convertView;
             if (v == null) {
                 LayoutInflater vi = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                v = vi.inflate(R.layout.checklist_list_row, null);
+                v = vi.inflate(R.layout.note_list_row, null);
             }
             
             
@@ -43,8 +90,8 @@ public class ReminderAdapter extends ArrayAdapter<ReminderType> {
                     if (tt != null) {
                           tt.setText("Title: "+o.getTitle());
                     }
-                    if(bt != null){
-                    	// TODO: Use activity helper
+                    if(bt != null)
+                    {
                 		DatabaseHelper h = OpenHelperManager.getHelper(context, DatabaseHelper.class); 
                      	String cattitle = h.getCategoryString(o);
                     	bt.setText("Category: "+ cattitle);
@@ -56,4 +103,13 @@ public class ReminderAdapter extends ArrayAdapter<ReminderType> {
     }
 
 
+
+    public void refreshData()
+    {
+
+    	reloadReminders();
+    	notifyDataSetChanged();
+    	
+    }
+    
 }
--- trunk/Organizer/src/edu/sru/nullstring/UI/ReminderMainActivity.java
+++ trunk/Organizer/src/edu/sru/nullstring/UI/ReminderMainActivity.java
@@ -1,21 +1,37 @@
 package edu.sru.nullstring.UI;
 
+import java.sql.SQLException;
+
+import com.j256.ormlite.android.apptools.OpenHelperManager;
 import com.j256.ormlite.android.apptools.OrmLiteBaseActivity;
 import com.j256.ormlite.android.apptools.OrmLiteSqliteOpenHelper;
 
 import edu.sru.nullstring.R;
 import edu.sru.nullstring.R.layout;
 import edu.sru.nullstring.Data.DatabaseHelper;
+import edu.sru.nullstring.Data.NoteAdapter;
+import edu.sru.nullstring.Data.NoteType;
+import edu.sru.nullstring.Data.ReminderAdapter;
+import edu.sru.nullstring.Data.ReminderType;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemLongClickListener;
 
 public class ReminderMainActivity extends OrmLiteBaseActivity<DatabaseHelper> {
 
+	private ListView mReminderView;
+	private DatabaseHelper helper;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +43,27 @@ public class ReminderMainActivity extends OrmLiteBaseActivity<DatabaseHelper> {
         setContentView(R.layout.reminder_main);
         GlobalHeaderView head = (GlobalHeaderView)findViewById(R.id.topBanner);
         head.setActivity(this);
+        
+        
+        mReminderView = (ListView)findViewById(R.id.reminderView);
+
+        // DB code
+        try {
+        	DatabaseHelper helper = OpenHelperManager.getHelper(this, DatabaseHelper.class); 
+			mReminderView.setAdapter(new ReminderAdapter(this,
+	                android.R.layout.simple_list_item_1, helper));
+        } catch(Exception ex)
+        {
+        	Log.e("ReminderMainActivity", "Some database stuff failed =/");
+        }
+        
+		mReminderView.setTextFilterEnabled(true);
+		
+		// attach list item click
+		mReminderView.setOnItemClickListener(mListClickListener); 
+		mReminderView.setOnItemLongClickListener(mListLongClickListener); 
+
+        
         
     	Button addItem = (Button)findViewById(R.id.addItem);
     	addItem.setOnClickListener(new OnClickListener() {
@@ -49,5 +86,96 @@ public class ReminderMainActivity extends OrmLiteBaseActivity<DatabaseHelper> {
 		if(h != null) h.refreshData();
 		
 	}
+	
+	private View lastView = null;
+	private ReminderType currentNote;
+    // Handle list long press clicks
+    OnItemClickListener mListClickListener = new OnItemClickListener() {
+    		public void onItemClick(AdapterView<?> parent, View v, int position, long id)
+    		{
+    			Log.i("NoteMainActivity:OnItemClickListener", String.valueOf(position));
+    			// last view, hide remove button again
+    			if(lastView != null)
+    			{
+    			
+    				lastView.setBackgroundColor(Color.WHITE);
+    				Button hider = (Button)lastView.findViewById(R.id.listRightButtons);
+    				hider.setVisibility(View.GONE);
+    			}
+
+    			// current view, open it
+    			if(v != null)
+    			{
+        			ReminderAdapter nada = (ReminderAdapter) parent.getAdapter();
+        			currentNote = nada.getItem(position);
+
+        			openEditorActivity(currentNote);
+    			}
+    		}
+    };
+    // Handle list long press clicks
+    OnItemLongClickListener mListLongClickListener = new OnItemLongClickListener() {
+    		public boolean onItemLongClick(AdapterView<?> parent, View v, int position, long id)
+    		{
+    			Log.i("NoteMainActivity:OnItemClickListener", String.valueOf(position));
+    			// last view, hide remove button again
+    			if(lastView != null)
+    			{
+    			
+    				lastView.setBackgroundColor(Color.WHITE);
+    				Button hider = (Button)lastView.findViewById(R.id.listRightButtons);
+    				hider.setVisibility(View.GONE);
+    			}
+
+
+    			// current view, make remove button visible
+    			if(v != null)
+    			{
+        			ReminderAdapter nada = (ReminderAdapter) parent.getAdapter();
+        			currentNote = nada.getItem(position);
+    				v.setBackgroundColor(Color.LTGRAY);
+    				Button hider = (Button)v.findViewById(R.id.listRightButtons);
+    				hider.setOnClickListener(new OnClickListener() {
+    						public void onClick(View v)
+    						{
+    			    			if(lastView != null)
+    			    			{
+    			    				lastView.setBackgroundColor(Color.WHITE);
+    			    				Button hider = (Button)lastView.findViewById(R.id.listRightButtons);
+    			    				hider.setVisibility(View.GONE);
+    			    			};
+    							remove(currentNote);
+    						}
+    					});
+    				hider.setVisibility(View.VISIBLE);
+    				lastView = v;
+    			}
+        		
+        		return true;
+    		}
+    };
+
+	public void openEditorActivity(ReminderType item)
+	{
+        Intent myIntent = new Intent(this, ReminderNewActivity.class);
+        myIntent.putExtra("edu.sru.nullstring.reminderId", item.getID());
+        startActivityForResult(myIntent, 0);
+	}
+
+	
+    public boolean remove(ReminderType note)
+    {
+    	try {
+    		// delete item
+			note.delete();
+			// Refresh list
+			((ReminderAdapter)mReminderView.getAdapter()).refreshData();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+	    	return false;
+		}
+    	return true;
+    }
 
 }
