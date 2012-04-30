242c8c288c37cad094ae90d023e7530d75087739
spackjlefish@gmail.com <spackjlefish@gmail.com@ae435935-a78b-c63e-f1f1-0351114e69a1>
2012-04-30 14:33:55 +0000

    BSD License, Calendar, Keystore

--- trunk/Organizer/src/edu/sru/nullstring/Data/ReminderType.java
+++ trunk/Organizer/src/edu/sru/nullstring/Data/ReminderType.java
@@ -1,3 +1,29 @@
+/*
+ * Copyright (c) 2012, SRU Cygnus Nullstring.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, 
+are permitted provided that the following conditions are met:
+
+*     Redistributions of source code must retain the above copyright notice, 
+		this list of conditions and the following disclaimer.
+	
+*     Redistributions in binary form must reproduce the above copyright notice, 
+     	this list of conditions and the following disclaimer in the documentation 
+     	and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
+EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF 
+THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+ */
+
+
 package edu.sru.nullstring.Data;
 
 import java.sql.SQLException;
@@ -5,6 +31,7 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.List;
+import java.util.TimeZone;
 
 import com.j256.ormlite.dao.Dao;
 import com.j256.ormlite.field.DatabaseField;
@@ -35,6 +62,8 @@ public class ReminderType extends BaseDaoEnabled<ReminderType, Integer> {
 		long derivTime = compareTo.getTime() - nowDate.getTime();
 		Calendar cal = Calendar.getInstance();
 		Date derivDate = new Date(derivTime);
+		// base on GMT
+		cal.setTimeZone(TimeZone.getTimeZone("GMT"));
 		cal.setTime(derivDate);
 		StringBuilder build = new StringBuilder();
 		
